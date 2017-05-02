a827744e5f783e0c74b20e22750d0906f8be736d
Tonjun Banaag <tonjun@gmail.com>
2017-05-02 18:15:04 +0800

    Rename pubsubmain to pubsub, add message pkg

---
+++ message/message.go
@@ -0,0 +1,45 @@
+package message
+
+/*
+{
+  "type": "subscribe",
+  "id": "reqid1",
+  "topics": [ "topic1", "topic2" ],
+}
+*/
+
+/*
+{
+  "type": "publish",
+  "id": "reqid1",
+  "topics": [ "topic1" ],
+  "payload": "abcdef",
+  "sender": {
+    "name": "Bob"
+  },
+}
+*/
+
+/*
+{
+  "type": "join",
+  "id": "xxxx",
+  "topics": [ "topic1" ],
+  "sender": {
+    "id": "bob123",
+    "name": "Bob",
+  }
+}
+*/
+
+/*
+{
+  "type": "leave",
+  "id": "xxxx",
+  "topics": [ "topic1" ],
+  "sender": {
+    "id": "bob123",
+    "name": "Bob",
+  }
+}
+*/
--- pubsubmain/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/tonjun/pubsub/pubsubsvr"
-)
-
-func main() {
-
-}
