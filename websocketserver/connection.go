f6c9b4417b92e71019d9df6a19084357efda7ded
Tonjun Banaag <tonjun@gmail.com>
2017-05-03 16:53:19 +0800

    Add handling of closed socket

--- websocketserver/connection.go
+++ websocketserver/connection.go
@@ -3,25 +3,33 @@ package websocketserver
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 type Connection struct {
-	id   int64
-	ws   *websocket.Conn
-	send chan []byte
+	id       int64
+	ws       *websocket.Conn
+	send     chan sendRequest
+	closed   bool
+	closedMu sync.RWMutex
 
 	onCloseCb func(c *Connection)
 	onMesgCb  func(c *Connection, b []byte)
+}
+
+type sendRequest struct {
+	data  []byte
+	errCh chan error
 }
 
 func NewConnection(id int64, ws *websocket.Conn) *Connection {
 	return &Connection{
 		id:   id,
 		ws:   ws,
-		send: make(chan []byte),
+		send: make(chan sendRequest),
 	}
 }
 
@@ -31,18 +39,36 @@ func (c *Connection) ID() int64 {
 
 func (c *Connection) Send(data []byte) error {
 	log.Printf("Send: \"%s\"", string(data))
-
+	if c.isClosed() {
+		return fmt.Errorf("connection closed")
+	}
+	req := &sendRequest{
+		data:  data,
+		errCh: make(chan error),
+	}
 	select {
-	case c.send <- data:
+	case c.send <- *req:
 	case <-time.After(1 * time.Second):
 		log.Printf("Send: timeout!!")
 		return fmt.Errorf("Timeout while sending to channel")
 	}
-
-	return nil
+	err := <-req.errCh
+	return err
 }
 
 func (c *Connection) Close() {
+	log.Printf("Close")
+	defer log.Printf("Close done")
+	if c.isClosed() {
+		log.Printf("Close: already closed")
+		return
+	}
+	c.closedMu.Lock()
+	c.closed = true
+	c.closedMu.Unlock()
+	c.ws.Close()
+	c.onCloseCb(c)
+	close(c.send)
 }
 
 func (c *Connection) OnClose(fn func(c *Connection)) {
@@ -60,24 +86,39 @@ func (c *Connection) write(mt int, payload []byte) error {
 func (c *Connection) writePump() {
 	defer func() {
 		log.Printf("writePump: done")
+		c.Close()
+		//close(c.send)
 	}()
 	for {
 		select {
-		case message, ok := <-c.send:
+		case req, ok := <-c.send:
 			if !ok {
 				log.Printf("writePump: send channel !ok")
 				c.write(websocket.CloseMessage, []byte{})
+				if req.errCh != nil {
+					req.errCh <- fmt.Errorf("send channel !ok")
+				} else {
+					log.Printf("req.errCh is nil")
+				}
 				return
 			}
-			if err := c.write(websocket.TextMessage, message); err != nil {
-				log.Printf("WriteMessage error: %s", err.Error())
+			if err := c.write(websocket.TextMessage, req.data); err != nil {
+				log.Printf("writePump: WriteMessage error: %s", err.Error())
+				req.errCh <- err
 				return
 			}
+			req.errCh <- nil
 		}
 	}
 }
 
 func (c *Connection) readPump() {
+	defer func() {
+		//c.ws.Close()
+		//c.onCloseCb(c)
+		c.Close()
+		log.Printf("readPump: done")
+	}()
 	for {
 		t, message, err := c.ws.ReadMessage()
 		if err != nil {
@@ -89,6 +130,10 @@ func (c *Connection) readPump() {
 			c.onMesgCb(c, message)
 		}
 	}
-	c.ws.Close()
-	c.onCloseCb(c)
+}
+
+func (c *Connection) isClosed() bool {
+	c.closedMu.RLock()
+	defer c.closedMu.RUnlock()
+	return c.closed
 }
--- websocketserver/connection_test.go
+++ websocketserver/connection_test.go
@@ -92,3 +92,87 @@ func TestConnectionSend(t *testing.T) {
 	time.Sleep(100 * time.Millisecond)
 
 }
+
+func TestConnectionSendOnClosed(t *testing.T) {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	// manage connections
+	added := make(chan pubsub.Conn)
+	closed := make(chan pubsub.Conn)
+	send := make(chan string)
+	connections := make(map[int64]pubsub.Conn)
+	go func() {
+		for {
+			select {
+			case c := <-added:
+				connections[c.ID()] = c
+			case c := <-closed:
+				log.Printf("deleting connection id: %d", c.ID())
+				delete(connections, c.ID())
+
+			case m := <-send:
+				for _, c := range connections {
+					for i := 0; i < 10; i++ {
+						go func(con pubsub.Conn) {
+							err := con.Send([]byte(m))
+							if err != nil {
+								log.Printf("TEST Send errror: %s", err.Error())
+							}
+							//assert.Nil(t, err)
+						}(c)
+					}
+				}
+			}
+		}
+	}()
+
+	// create server and add callbacks
+	var srv pubsub.Server
+	addr := ":7070"
+	srv = NewWebSocketServer(&Options{
+		ListenAddr: addr,
+		Pattern:    "/ws",
+	})
+	assert.NotNil(t, srv)
+	srv.OnConnectionAdded(func(conn pubsub.Conn) {
+		added <- conn
+	})
+	srv.OnConnectionWillClose(func(conn pubsub.Conn) {
+		//closed <- conn
+	})
+	go srv.Run()
+	time.Sleep(10 * time.Millisecond)
+
+	// connect to the server
+	wt := wstester.NewWSTester(&wstester.Options{
+		ServerAddr: fmt.Sprintf("ws://localhost%s/ws", addr),
+		Count:      1,
+	})
+	wt.Start()
+
+	send <- "hello world Closed socket"
+
+	time.Sleep(100 * time.Millisecond)
+
+	conns, _ := wt.GetConnections()
+	for _, c := range conns {
+		assert.Equal(t, 10, len(c.Messages))
+		for _, m := range c.Messages {
+			//log.Printf("%d messageList: %s", i, string(c.Messages[0]))
+			assert.Equal(t, "hello world Closed socket", string(m))
+		}
+	}
+	conns[0].WS.Close()
+
+	time.Sleep(100 * time.Millisecond)
+
+	send <- "hello world Closed socket"
+
+	// stop the server
+	srv.Stop()
+	time.Sleep(100 * time.Millisecond)
+
+	wt.Stop()
+	time.Sleep(100 * time.Millisecond)
+
+}
