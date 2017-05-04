77ac2b38f5155cc8e84bab82b6248afdebcd43fb
Tonjun Banaag <tonjun@gmail.com>
2017-05-04 15:58:28 +0800

    Add some config params

--- config.go
+++ config.go
@@ -5,9 +5,21 @@ import (
 	"os"
 )
 
+// Config is the pubsub server's configuration
 type Config struct {
+	Addr string     `json:"addr"` // TCP address to listen on
+	Path string     `json:"path"` // websocket path. e.g. "/ws"
+	TLS  *TLSConfig `json:"tls"`  // optional TLS config
 }
 
+// TLSConfig is used to configure a TLS server
+type TLSConfig struct {
+	Addr string `json:"addr"` // TLS listen address
+	Cert string `json:"cert"` // certificate file path
+	Key  string `json:"key"`  // private key file path
+}
+
+// NewConfig returns a new Config given the config file path
 func NewConfig(file string) *Config {
 	f, err := os.Open(file)
 	if err != nil {
